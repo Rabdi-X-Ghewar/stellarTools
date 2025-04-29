8a4e320c10f563f99aabfefd2567d2a110f19c6f
Goutam-11 <goutamkumar.sharmq@gmail.com>
2025-04-29 23:27:31 +0530

    types declaration added

--- index.ts
+++ index.ts
@@ -2,10 +2,9 @@ import { bridgeTokenTool } from "./tools/bridge";
 import { StellarLiquidityContractTool } from "./tools/contract";
 import { StellarContractTool } from "./tools/stake";
 import { stellarSendPaymentTool } from "./tools/stellar";
-
 export const stellarTools = [
   bridgeTokenTool,
   StellarLiquidityContractTool,
   StellarContractTool,
   stellarSendPaymentTool
-];
+]; 
